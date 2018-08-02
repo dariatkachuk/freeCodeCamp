fae38f2b4776c5017d90a42a2b4cf5e36e0825e9
Daria Tkachuk <daryasmail@gmail.com>
2018-08-02 20:31:50 +0300

    Array methods filter and map

---
+++ Functional-programming/filter-prototype-2.js
@@ -0,0 +1,20 @@
+/* Functional Programming: Implement the filter Method on a Prototype */
+
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  this.forEach(elem => {
+    if (callback(elem)) {
+      newArray.push(elem);
+    }
+  }) 
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
